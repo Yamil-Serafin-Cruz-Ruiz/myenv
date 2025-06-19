af585ea270c488143f8f79a88298264b2a6798e7
Yamil Serafin Cruz Ruiz <20210656@uthh.edu.mx>
2025-06-18 21:00:39 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from joblib import load
+
+app = Flask(__name__)
+modelo = load('../bichos/modelobichos.pkl')
+
+@app.route('/')
+def home():
+    return "¡Hola, Mundo!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    datos = request.get_json()
+    prediccion = modelo.predict([datos['features']])
+    return jsonify({'resultado': int(prediccion[0])})
+
